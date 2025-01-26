Clean up and numerical bug fix

file_comparison/iterables.py
@@ -48,7 +48,7 @@ def contains_values (container):
 
 def compare_lists (list1:list, list2:list, comparison_path: str, block_diff: dict ):
 
-    print ("iterable_are_equal List")
+    # print ("iterable_are_equal List")
     # block_diff["log"].append(comparison_path+str(type(list1)))
     if len(list1) != len(list2):
         block_diff["error"].append(str(comparison_path+str(type(list1))+"->") + "List don't have same length")
@@ -72,7 +72,7 @@ def compare_dicts (original_item, new_item, comparison_path, block_diff):
     keys_to_avoid = []
     common_keys = []
     # block_diff["log"].append (comparison_path+str(type(original_item)))
-    print ("iterable_are_equal Dict")
+    # print ("iterable_are_equal Dict")
 
 
     for ikey in original_item.keys():
@@ -97,98 +97,6 @@ def compare_dicts (original_item, new_item, comparison_path, block_diff):
     return block_diff
     
 
-# def compare_numpy_arrays (original_item, new_item, comparison_path, block_diff):
-
-#     block_diff["log"].append(comparison_path+str(type(original_item)))
-#     block_diff["nvalues"] += len(original_item)
-#     block_diff["nerrors"] += abs(len(original_item) - len(new_item))
-    
-#     # Check sizes
-#     # TODO
-
-#     # Check data types
-#     # TODO
-
-#     block_diff["report"].append(file_comparison.report_generator.compute_1list_difference(origin=original_item, new=new_item))
-    
-#     # Add errors and logs
-#     # TODO
-#     block_diff["nerrors"] += len(original_item) - len(new_item)
-    
-#     # block_diff = iterable_are_equal(original_item.tolist(), new_item.tolist(), comparison_path+str(type(original_item))+"->", block_diff)
-
-#     return block_diff
-
-# def compare_numpy_npz (original_item, new_item, comparison_path, block_diff):
-
-#     keys_to_avoid = []
-#     common_keys = []
-
-#     # Check keys_to_avoid# # TODO
-#     for ikey in original_item.files:
-#         if not ikey in new_item.files:
-#             keys_to_avoid.append(ikey)
-#         elif not ikey in common_keys:
-#             common_keys.append(ikey)
-
-#     for ikey in new_item.files:
-#         if not ikey in original_item.files:
-#             keys_to_avoid.append(ikey)
-#         elif not ikey in common_keys:
-#             common_keys.append(ikey)
-
-#     # common_keys = original_item.files - keys_to_avoid
-#     if len(keys_to_avoid) > 0:
-#         block_diff["error"].append(str(comparison_path+str(type(original_item))+"->KeysAvoided") + str( keys_to_avoid))
-#         block_diff["nerrors"] += len(keys_to_avoid)
-#         block_diff["nvalues"] += len(keys_to_avoid)
-
-#     # Iterate on keys
-#     for ivar in common_keys:
-#         block_diff = iterable_are_equal(original_item[ivar], new_item[ivar], comparison_path+str(type(original_item))+"->"+str(ivar)+"->", block_diff)
-#     return block_diff
-
-
-# 4
-# def compute_score (number_of_errors, number_of_values):
-#     #TODO Catch Exception instead of assert
-#     assert number_of_values > 0, "No data to compare, score is divided by 0"
-#     print ("Number of errors = " + str (number_of_errors))
-#     print ("Number of values = " + str (number_of_values))
-#     # print ("Number of failures = " + str (len(self.all_failures)) + "\n")
-
-#     score = 100. - (number_of_errors*100./number_of_values)
-
-#     return (score)
-
-# 3
-# def compute_differences_report (original_file, new_file):
-
-#     block_diff = {"report": [], "nerrors": 0, "nvalues": 0, "log": [], "error": []}
-#     comparison_path = "R"
-#     try:
-#         original_data = np.load(original_file["path"], allow_pickle=original_file["allow_pickle"], encoding=original_file["encoding"])
-#         new_data = np.load(new_file["path"], allow_pickle=new_file["allow_pickle"], encoding=new_file["encoding"])
-
-#         block_diff = iterable_are_equal (original_data, new_data, comparison_path, block_diff)
-#         # print (block_diff)
-#         # print ("\n")
-
-#     except Exception as e:
-#         block_diff["error"].append("NPZ compute_differences_report: " + str(e))
-#         block_diff["nerrors"] += 1
-
-#     return block_diff
-
-# 2
-# def check_file_formats (filepath):
-#     try:
-#         np.load(filepath, allow_pickle=True)
-#         return True, None
-#     except Exception as e:
-#         print ("Error " + str(type(e)) + " :: NPZ method: " + str(e))
-#         return False, str(e)
-
 def iterable_are_equal (original_item, new_item, comparison_path, block_diff):
     
     if (type (original_item) not in known_types or type(new_item) not in known_types):
@@ -197,32 +105,19 @@ def iterable_are_equal (original_item, new_item, comparison_path, block_diff):
         block_diff ["error"].append(comparison_path + " " + str(type(original_item)) + " " + str(type(new_item)) + " are not in KNOWN Types")
         block_diff["nerrors"]+=1
         block_diff["nvalues"]+=1
-        print ("iterable_are_equal unknown types " + str(type (original_item)) + " -- " + str(type (new_item)))
+        # print ("iterable_are_equal unknown types " + str(type (original_item)) + " -- " + str(type (new_item)))
 
     #############   NUMPY.NPZ.Files  #################
     # Convert npz files into compatible arrays
     if ((type(original_item) == np.lib.npyio.NpzFile) and (type(new_item) == np.lib.npyio.NpzFile)):
-
-        print ("iterable_are_equal NPZ type")
+        # print ("iterable_are_equal NPZ type")
 
         block_diff = file_comparison.npz.compare_numpy_npz (original_item, new_item, comparison_path+str(type(original_item))+"->", block_diff)
 
     #############   NUMPY.arrays  #################
     # Convert numpy arrays into compatible arrays
     elif ((type(original_item) == np.ndarray) and (type(new_item) == np.ndarray)):
-        print ("iterable_are_equal Numpy Array")
-
-        # # Check type similar
-        # if (original_item.dtype != new_item.dtype):
-        #     block_diff["error"].append(comparison_path+str(type(original_item)) + ": Different data types")
-        #     block_diff["nerrors"] += abs(len(original_item))
-        # else:
-            # Check element types are primitive types
-            # if (original_item.dtype not in container_types):
-            #     print ("iterable_are_equal Numpy Array not in container types:  " + str(original_item.dtype))
-            #     block_diff = file_comparison.npz.compare_numpy_arrays (original_item, new_item, comparison_path+str(type(original_item))+"->", block_diff)
-            # else:
-                # print ("iterable_are_equal Numpy Array type " + str(original_item.dtype))
+        # print ("iterable_are_equal Numpy Array")
                 
         # Check array length
         if len(original_item) - len(new_item):
@@ -237,19 +132,19 @@ def iterable_are_equal (original_item, new_item, comparison_path, block_diff):
     # TODO
     elif (type(original_item) == neo.core.block.Block) and (type(new_item) == neo.core.block.Block):
         # TODO
-        print ("iterable_are_equal NEO Block")
+        # print ("iterable_are_equal NEO Block")
         block_diff = file_comparison.neo.compare_neo_blocks (original_item, new_item, comparison_path+str(original_item.name)+str(type(original_item))+"->", block_diff)
 
     ############    NEO.SEGMENT ##################
     # TODO
     elif (type(original_item) == neo.core.Segment) and (type(new_item) == neo.core.Segment):
-        print ("iterable_are_equal NEO Segment")
+        # print ("iterable_are_equal NEO Segment")
         block_diff = file_comparison.neo.compare_segments(original_item, new_item, comparison_path+str(original_item.name)+str(type(original_item))+"->", block_diff)
         
     elif ((isinstance(original_item, Iterable)) and (isinstance(new_item, Iterable)) and (type(original_item)!=str) and (type(original_item)!= bytes) ):
 
 
-        print ("iterable_are_equal Iterable type")
+        # print ("iterable_are_equal Iterable type")
         #################   LIST    ###################
         if (((type(original_item) == list) and (type(new_item) == list)) or ((type(original_item) == tuple) and (type(new_item) == tuple))):
             # Check array length

file_comparison/report_generator.py
@@ -19,7 +19,7 @@ def compute_1list_difference (origin:np.ndarray, new:np.ndarray):
     # Test mean delta
     # Compute Mean Absolute difference between two values
     try:
-        block_diff_1list["delta"] = stats.delta(origin, new) 
+        block_diff_1list["delta"] = stats.delta(origin, new).item()
         # block_diff_1list["delta"] = np.nanmean(np.absolute(origin - new))
     except Exception as e:
         block_diff_1list["log"].append("Mean Delta Stat: " + str("".join(traceback.format_exception(e))))
@@ -28,7 +28,7 @@ def compute_1list_difference (origin:np.ndarray, new:np.ndarray):
     # Test maximum delta
     # Compute Maximum difference in dataset
     try:
-        block_diff_1list["max delta"] = stats.maximum_delta(origin, new)
+        block_diff_1list["max delta"] = stats.maximum_delta(origin, new).item()
     except Exception as e:
         block_diff_1list["log"].append("Max Delta Stat:" + str("".join(traceback.format_exception(e))))
         block_diff_1list["max delta"] = None
@@ -36,7 +36,7 @@ def compute_1list_difference (origin:np.ndarray, new:np.ndarray):
     # Test string values
     # Compute Levenshtein distance percentage between two strings
     try:
-        block_diff_1list["levenshtein"] = stats.mean_levenshtein_distance_percentage(origin, new)
+        block_diff_1list["levenshtein"] = stats.mean_levenshtein_distance_percentage(origin, new).item()
     except Exception as e:
         block_diff_1list["log"].append("Levenshtein Stat: " + str("".join(traceback.format_exception(e))))
         block_diff_1list["levenshtein"] = None
@@ -44,7 +44,7 @@ def compute_1list_difference (origin:np.ndarray, new:np.ndarray):
     # Test mape
     # Compute Absolute Percentage Error between two values
     try:
-        block_diff_1list["mape"] = stats.mean_absolute_percentage_error(origin, new)
+        block_diff_1list["mape"] = stats.mean_absolute_percentage_error(origin, new).item()
     except Exception as e:
         block_diff_1list["log"].append("MAPE Stat: " + str("".join(traceback.format_exception(e))))
         block_diff_1list["mape"] = None
@@ -53,7 +53,7 @@ def compute_1list_difference (origin:np.ndarray, new:np.ndarray):
     # Compute Mean Squared Percentage Error between two values
     # TODO
     try:
-        block_diff_1list["mspe"] = stats.mean_squared_percentage_error(origin, new)        
+        block_diff_1list["mspe"] = stats.mean_squared_percentage_error(origin, new).item()        
     except Exception as e:
         block_diff_1list["log"].append("MSPE Stat: " + str("".join(traceback.format_exception(e))))
         block_diff_1list["mspe"] = None
@@ -61,7 +61,7 @@ def compute_1list_difference (origin:np.ndarray, new:np.ndarray):
     # Test rmspe
     # Compute Root Mean Squared Percentage Error between two lists
     try:
-        block_diff_1list["rmspe"] = stats.root_mean_squared_percentage_error(origin, new)        
+        block_diff_1list["rmspe"] = stats.root_mean_squared_percentage_error(origin, new).item()        
     except Exception as e:
         block_diff_1list["log"].append("RMSPE Stat: " + str("".join(traceback.format_exception(e))))
         block_diff_1list["rmspe"] = None
@@ -69,7 +69,7 @@ def compute_1list_difference (origin:np.ndarray, new:np.ndarray):
     # Test mpe
     # Compute Mean Percentage Error between two lists
     try:
-        block_diff_1list["mpe"] = stats.mean_percentage_error(origin, new)
+        block_diff_1list["mpe"] = stats.mean_percentage_error(origin, new).item()
     except Exception as e:
         block_diff_1list["log"].append("MPE Stat: " + str("".join(traceback.format_exception(e))))
         block_diff_1list["mpe"] = None
@@ -77,7 +77,7 @@ def compute_1list_difference (origin:np.ndarray, new:np.ndarray):
     # Test rpd
     # Compute Relative Percentage Difference between two lists
     try:
-        block_diff_1list["rpd"] = stats.mean_relative_percentage_difference(origin, new)
+        block_diff_1list["rpd"] = stats.mean_relative_percentage_difference(origin, new).item()
     except Exception as e:
         block_diff_1list["log"].append("RPD Stat: " + str("".join(traceback.format_exception(e))))
         block_diff_1list["rpd"] = None
@@ -85,7 +85,7 @@ def compute_1list_difference (origin:np.ndarray, new:np.ndarray):
     # Test nilsimsa
     # Compute Nilsimsa Distane between two lists
     try:
-        block_diff_1list["nilsimsa"] = stats.mean_nilsimsa_distance(origin, new)
+        block_diff_1list["nilsimsa"] = stats.mean_nilsimsa_distance(origin, new).item()
     except Exception as e:
         block_diff_1list["log"].append("Mean Nilsimsa Stat: " + str("".join(traceback.format_exception(e))))
         block_diff_1list["nilsimsa"] = None