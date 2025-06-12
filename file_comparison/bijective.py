9f923ec6f5a267aa5f235757c77bd1b04e017d85
fbonnier <florent.bonnier@cnrs.fr>
2025-06-12 10:25:30 +0200

    Bug fix

--- file_comparison/bijective.py
+++ file_comparison/bijective.py
@@ -29,6 +29,28 @@ def compute_ratio (score):
 
 def find_bijective (produced_outputs, expected_outputs):
 
+    # # All files scores
+    # all_file_scores = [[None for _ in expected_outputs] for _ in produced_outputs]
+
+    # # Compute scores for all pairs of files
+    # for ifile1 in produced_outputs:
+    #     for ifile2 in expected_outputs:
+    #         all_file_scores[produced_outputs.index(ifile1)][expected_outputs.index(ifile2)] = compute_ratio(compare_digests(Nilsimsa(ifile1["filename"] + str(ifile1["size"])).hexdigest(), Nilsimsa(ifile2["filename"] + str(ifile2["size"])).hexdigest()))
+
+    # # Compute the maximum score for each produced file and check if it is bijective
+    # for ifile1 in produced_outputs:
+    #     partner = {"ifile2": None, "score": 0}
+    #     # Search for nearest partner
+    #     for ifile2 in expected_outputs:
+    #         new_score = all_file_scores[produced_outputs.index(ifile1)][expected_outputs.index(ifile2)]
+    #         if partner["score"] < new_score:
+    #             # Search for the nearest of the nearest
+    #             for ifile3 in produced_outputs:
+    #                 best_score_ifile2 = all_file_scores[produced_outputs.index(ifile3)][expected_outputs.index(ifile2)]
+    #                 if best_score_ifile2 == new_score:
+    #             partner["ifile2"] = ifile2
+    #             partner["score"] = new_score 
+
     # Walk the files and build blocks of pairs
     blocks_of_pairs = [] 
     for ifile1 in produced_outputs:
@@ -40,9 +62,9 @@ def find_bijective (produced_outputs, expected_outputs):
                 partner["ifile2"] = ifile2
                 partner["score"] = new_score
         block = {"Origin": None, "New": None, "hash score": None, "format": None, "error": [], "method": None, "log":[], "advice": [], "score": []}
-        block["Origin"] = ifile1
+        block["Origin"] = partner["ifile2"]
         block["Origin"]["origin"] = "expected"
-        block["New"] = partner["ifile2"]
+        block["New"] = ifile1
         block["New"]["origin"] = "produced"
         block["bijective score"] = partner["score"]*100
 
